86277e02756203215fb4aef3d53884bce5d50bfe
nw11 <nw11@sanger.ac.uk>
2015-11-30 00:31:12 +0000

    export type CpGMethEstimate

--- src/MethTest.jl
+++ src/MethTest.jl
@@ -5,9 +5,11 @@ module MethTest
 export test_greater_population
 include("hyper-geometric.jl")
 
+
 export get_beta_parameters
 export getDCpGStatistic
 export CpGMethCount
+export CpGMethEstimate
 include("beta-binomial-z.jl")
 
 end # module
